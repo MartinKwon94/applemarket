762cd33808da7a35d475d79d4a3f53a9f5006466
Martin Kwon <mindori9097@gmail.com>
2023-08-29 11:05:38 +0900

    [Feature] list.xml UI디자인

--- app/src/main/java/com/example/applemarket/DetailActivity.kt
+++ app/src/main/java/com/example/applemarket/DetailActivity.kt
@@ -1,7 +1,7 @@
 package com.example.applemarket
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
